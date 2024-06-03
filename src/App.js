2f35a3f1be4a3fe39b019256c0c164d55959dc64
pyooz <real_pyj@naver.com>
2024-06-03 17:18:50 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ import PwdChange from "./page/User/PWD/PwdChange";
 import MyPage from "./page/User/MyPage";
 import IdCheck from "./page/User/ID/IdCheck";
 import LearningPage from "./page/Learn/LearningPage";
-import Nav from "./component/Nav";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from "./component/Nav";
 
 function App() {
   return (
--- src/page/Home.js
+++ src/page/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import ControlledCarousel from "../component/Carousel/Carousels";
+// import Nav from "../component/Nav.js";
 
 function Home() {
   const navigate = useNavigate();
