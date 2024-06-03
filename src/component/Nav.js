545d92e4cd046b5819642bd4281ca903304faa40
pyooz <real_pyj@naver.com>
2024-06-03 14:02:14 +0900

    Update Nav.js

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -9,18 +9,25 @@ function Nav() {
       <Link to={"/"}>
         <img className="logo" src={Logo} />
       </Link>
+
       <Link className="navbarMenu" to={"/"}>
         홈
       </Link>
+      <Link class="navbarMenu" to={"/post"}>커뮤니티</Link>
       <Link className="navbarMenu" to={"/learn"}>
         학습하기
       </Link>
-      {
-        /* <Link className="navbarMenu" to={'/Game'}>게임하기</Link> */
-        <Link className="navbarMenu" to={"/mypage"}>
-          마이페이지
-        </Link>
-      }
+      <Link className="navbarMenu" to={"/myPage"}>
+        마이페이지
+      </Link>
+      <Link class="navbarMenu" to={"/imageGame"}>이미지 게임</Link>
+      <Link class="navbarMenu" to={"/combineGame"}>낱말 조합</Link>
+      <Link class="navbarMenu" to={"/login"}>
+        로그인
+      </Link>
+      <Link class="navbarMenu" to={"/register"}>
+        회원가입
+      </Link>
     </div>
   );
 }
