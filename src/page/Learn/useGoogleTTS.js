f809f0b236e8b67f5522b609b4dbb29946da0c1a
pyooz <real_pyj@naver.com>
2024-06-05 01:34:04 +0900

    tts 변경 !!!

---
+++ src/page/Learn/useGoogleTTS.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useGoogleTTS = () => {
+  const apiKey = process.env.REACT_APP_GOOGLE_CLOUD_API_KEY;
+
+  const speak = async (text) => {
+    const url = `https://texttospeech.googleapis.com/v1/text:synthesize?key=${apiKey}`;
+
+    const data = {
+      input: { text },
+      voice: { languageCode: 'ko-KR', ssmlGender: 'NEUTRAL' },
+      audioConfig: { audioEncoding: 'MP3' },
+    };
+
+    try {
+      const response = await axios.post(url, data);
+      const audioContent = response.data.audioContent;
+      const audio = new Audio(`data:audio/mp3;base64,${audioContent}`);
+      audio.play();
+    } catch (error) {
+      console.error('텍스트 음성 변환 중 오류 발생:', error);
+    }
+  };
+
+  return speak;
+};
+
+export default useGoogleTTS;
