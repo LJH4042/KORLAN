3395b59234d01b2021dd04d23c3906c5f25a41c5
pyooz <real_pyj@naver.com>
2024-06-04 21:09:48 +0900

    모바일 화면 반응형 네비바 수정

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,5 +1,5 @@
 import { Link, NavLink } from "react-router-dom";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../css/Nav.css";
 import Logo from "../logo.png";
 import axios from "axios";
@@ -20,6 +20,24 @@ function Nav() {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
+  // 화면 너비가 480이하일 때 메뉴를 닫는 함수
+  const closeMenusOnMobile = () => {
+    if (window.innerWidth <= 480) {
+      setIsMobileMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 480) {
+        setIsMobileMenuOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className="navbar">
       <Link to={"/"}>
@@ -31,13 +49,13 @@ function Nav() {
       </div>
 
       <div className={`menuContainer ${isMobileMenuOpen ? 'open' : ''}`}>
-        <NavLink className="navbarMenu" exact to={"/"} activeClassName="active">
+        <NavLink className="navbarMenu" exact to={"/"} activeClassName="active" onClick={closeMenusOnMobile}>
           홈
         </NavLink>
-        <NavLink className="navbarMenu" to={"/post"} activeClassName="active">
+        <NavLink className="navbarMenu" to={"/post"} activeClassName="active" onClick={closeMenusOnMobile}>
           커뮤니티
         </NavLink>
-        <NavLink className="navbarMenu" to={"/learn"} activeClassName="active">
+        <NavLink className="navbarMenu" to={"/learn"} activeClassName="active" onClick={closeMenusOnMobile}>
           학습하기
         </NavLink>
         <div
@@ -47,25 +65,25 @@ function Nav() {
           게임하기
           {isDropdownOpen && (
             <div className="dropdown-content">
-              <NavLink to={"/imageGame"} onClick={resetGameData} activeClassName="active">
+              <NavLink to={"/imageGame"} onClick={(e) => { resetGameData(); closeMenusOnMobile(); }} activeClassName="active">
                 이미지 게임
               </NavLink>
-              <NavLink to={"/combineGame"} onClick={resetGameData} activeClassName="active">
+              <NavLink to={"/combineGame"} onClick={(e) => { resetGameData(); closeMenusOnMobile(); }} activeClassName="active">
                 낱말 조합
               </NavLink>
             </div>
           )}
         </div>
-        <NavLink className="navbarMenu" to={"/myPage"} activeClassName="active">
+        <NavLink className="navbarMenu" to={"/myPage"} activeClassName="active" onClick={closeMenusOnMobile}>
           마이페이지
         </NavLink>
       </div>
 
       <div className={`authContainer ${isMobileMenuOpen ? 'open' : ''}`}>
-        <NavLink className="navbarAuth" to={"/login"} activeClassName="active">
+        <NavLink className="navbarAuth" to={"/login"} activeClassName="active" onClick={closeMenusOnMobile}>
           로그인
         </NavLink>
-        <NavLink className="navbarAuth" to={"/register"} activeClassName="active">
+        <NavLink className="navbarAuth" to={"/register"} activeClassName="active" onClick={closeMenusOnMobile}>
           회원가입
         </NavLink>
       </div>
