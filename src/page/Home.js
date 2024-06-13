818faa59ca18be72cc6a1829724f2892892de4c3
pyooz <real_pyj@naver.com>
2024-06-13 23:58:31 +0900

    임시 카드

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ControlledCarousel from "../component/Carousel/Carousels";
+import Card from "../component/card";
 
 function Home() {
   return (
     <div>
       <ControlledCarousel />
+      <Card />
     </div>
   );
 }
