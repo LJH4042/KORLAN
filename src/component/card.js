dbf7290869ad14dbd28e2457c50bd5b5619a4dc8
pyooz <real_pyj@naver.com>
2024-08-17 20:45:22 +0900

    홈 및 소개

--- src/component/card.js
+++ src/component/card.js
@@ -5,25 +5,28 @@ const Card = () => {
   const cardData = [
     {
       title: '학습하기',
-      description: '학습하기상세',
-      link: '/learn'
+      description: '한글의 기초, 자음과 모음을 만나 보아요',
+      link: '/learn',
+      className: 'learn-card'
     },
     {
       title: '이미지 게임',
-      description: '이미지게임상세',
-      link: '/imageGame'
+      description: '다양한 사진을 통해 단어를 익혀 보아요',
+      link: '/imageGame',
+      className: 'image-card'
     },
     {
       title: '낱말 조합',
-      description: '낱말조합상세',
-      link: '/combineGame'
+      description: '자모음을 조합해 단어를 만들어 보아요',
+      link: '/combineGame',
+      className: 'combine-card'
     }
   ];
 
   return (
     <div className="card-container">
       {cardData.map((card, index) => (
-        <div key={index} className="card">
+        <div key={index} className={`card ${card.className}`}>
           <h3>{card.title}</h3>
           <p>{card.description}</p>
           <a href={card.link}>바로가기</a>
--- src/page/introduce.js
+++ src/page/introduce.js
@@ -3,10 +3,10 @@ import React from 'react';
 import '../css/introduce.css';
 
 const teamMembers = [
-  { name: '박유진', role: '프론트엔드 개발자' },
-  { name: '신윤호', role: '백엔드 개발자' },
-  { name: '이정호', role: '프론트엔드 개발자' },
-  { name: '최하은', role: '프론트엔드 개발자' },
+  { name: '박유진', role: '프론트엔드' },
+  { name: '신윤호', role: '백엔드' },
+  { name: '이정호', role: '프론트엔드' },
+  { name: '최하은', role: '프론트엔드' },
 ];
 
 const TeamMember = ({ name, role }) => (
@@ -23,6 +23,19 @@ const Introduce = () => (
       {teamMembers.map((member, index) => (
         <TeamMember key={index} {...member} />
       ))}
+    </div>
+    <div className='site-introduce'>
+      어린이들이 한글 자모와 기본 단어를 익히고, 재미있는 방식으로 한글을 사용할 수 있는 페이지를 만드는 게 목표였습니다.<br />
+      학습을 통해 자모의 정확한 발음을 들을 수 있고, 따라서 써볼 수도 있습니다. <br />
+      또한 이미지 게임과 단어 조합을 통해 단어를 응용하여 학습할 수 있습니다.
+    </div>
+    <div>
+      <div className='site-introduce'>
+        <div className='icon-container' onClick={() => window.open('https://github.com/LJH4042/KORLAN', '_blank')}>
+          <span className='icon'>🔗</span>
+        </div>
+        아이콘을 누르면 저희가 작업한 홈페이지의 소스 코드를 올려 둔 깃허브 주소를 확인하실 수 있습니다.
+      </div>
     </div>
   </div>
 );
