e70a8bda8eac92958dc4331ae8cdc83906350f55
yunho8493 <127362250+rozi8493@users.noreply.github.com>
2024-09-18 00:37:16 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import LearningPage from "./page/Learn/LearningPage";
 import Footer from "./component/footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Introduce from "./page/introduce";
-import Introduce from "./page/notebook";
 import ImageLevel from "./page/Game/ImageLevel";
 import CombineLevel from "./page/Game/CombineLevel";
 import axios from "axios";
@@ -58,7 +57,6 @@ function App() {
           <Route path="/combineGame" element={<CombineLevel />} />
           <Route path="/learn" element={<LearningPage />} />
           <Route path="/introduce" element={<Introduce />} />
-          <Route path="/notebook" element={<Notebook />} />
         </Routes>
       </BrowserRouter>
       <Footer />
